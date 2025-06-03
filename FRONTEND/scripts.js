1aef837ea4b11901f5271144c45b94ebaa334f41
UillianAraujo <uillsilva52@gmail.com>
2025-06-03 19:27:42 -0300

    Commit inicial

---
+++ FRONTEND/scripts.js
@@ -0,0 +1,5 @@
+// Adicionar funcionalidades como validação de formulários, interações dinâmicas, etc.
+
+document.addEventListener("DOMContentLoaded", function() {
+    console.log("Site Magé Verde Online carregado!");
+});

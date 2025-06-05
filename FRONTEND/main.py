383a244385f0f0263f5ce41be1007ee6851ac018
UillianAraujo <uillsilva52@gmail.com>
2025-06-05 19:47:01 -0300

    Notas de login

--- FRONTEND/main.py
+++ FRONTEND/main.py
@@ -1,4 +1,30 @@
-from flask import Flask, render_template, redirect
+from Flask import Flask, render_template, redirect, request
 
 app = Flask(__name__)
 app.config['SECRET_KEY'] = 'UILLIANARAUJO'
+
+
+@app.route('/')
+def home():
+    return render_template('inicio.html')
+
+#Rota do loginUsuario
+
+@app.route('/loginUsuario', methods=['POST'])
+def loginUsuario():
+
+    nome = request.form.get('username')
+    senha = request.form.get('password')
+
+    print(nome, senha)
+    return redirect('/loginUsuario')
+
+#Rota do loginAdmin
+
+
+
+
+
+
+if __name__ == '__main__':
+    app.run(debug=True)

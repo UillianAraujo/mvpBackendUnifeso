c7d77610c4bd04e41b2ca80018b749eb8a5adcc9
UillianAraujo <uillsilva52@gmail.com>
2025-06-17 18:52:54 -0300

    SQLAlchemy

--- mageVerde/__init__.py
+++ mageVerde/__init__.py
@@ -1,9 +1,12 @@
 from flask import Flask
-
+from flask_sqlalchemy import SQLAlchemy
 
 
 app = Flask(__name__)
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///mageverde.db'
 
+
+database = SQLAlchemy(app)
 
 
 

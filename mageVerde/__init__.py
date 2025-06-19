003e744d8dbfcf0c66bff8f16edab07b3611a4cd
UillianAraujo <uillsilva52@gmail.com>
2025-06-19 19:51:05 -0300

    #como esconder dados sensíveis

--- mageVerde/__init__.py
+++ mageVerde/__init__.py
@@ -6,6 +6,13 @@ from flask_bcrypt import Bcrypt
 
 app = Flask(__name__)
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///mageverde.db'
+# lembrar de antes de subir 'secret_key' para o github, criar .gitignore
+# e adicionar o arquivo .env com a variável SECRET_KEY
+# substituir a linha da chave secreta por: 
+# import os
+# from dotenv import load_dotenv
+# load_dotenv()
+# app.config['SECRET_KEY'] = os.getenv('SECRET_KEY')
 app.config['SECRET_KEY'] = 'd679ee02248df5a0448ff4e4a67715cb'
 
 database = SQLAlchemy(app)

d12fd5a14a11bfb2ba90c987b2923703807dd7e5
UillianAraujo <uillsilva52@gmail.com>
2025-06-19 19:17:41 -0300

    inicio forms

--- mageVerde/forms.py
+++ mageVerde/forms.py
@@ -1,1 +1,14 @@
 # criar os formularios do site
+
+class LoginUsuarioForm():
+    pass
+
+class LoginAdmForm():
+    pass
+
+# não pretendo criar um template/formulário para cadastrar usuários.
+class CadastroForm():
+    pass
+
+class AreaAdmForm():
+    pass

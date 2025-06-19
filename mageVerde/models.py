5839996da83e89ae6e6427c180e63824bdf28d9f
UillianAraujo <uillsilva52@gmail.com>
2025-06-19 18:29:27 -0300

    add bancoDados adm

--- mageVerde/models.py
+++ mageVerde/models.py
@@ -8,3 +8,9 @@ class Usuario(database.Model):
     email = database.Column(database.String(50), unique=True, nullable=False)
     senha = database.Column(database.String(20), nullable=False)
 
+class Adm(database.Model):
+    id = database.Column(database.Integer, primary_key=True)
+    username = database.Column(database.String(50), nullable=False)
+    email = database.Column(database.String(50), unique=True, nullable=False)
+    matricula = database.Column(database.Integer, unique=True, nullable=False)
+    senha = database.Column(database.String(20), nullable=False)

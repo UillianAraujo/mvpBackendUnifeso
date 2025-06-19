69418385ff2a33de0dc958ad15216153d41a34ba
UillianAraujo <uillsilva52@gmail.com>
2025-06-19 18:55:20 -0300

    login_manager

--- mageVerde/models.py
+++ mageVerde/models.py
@@ -1,6 +1,13 @@
 # criar a estrutura do banco de dados
 
-from mageVerde import database
+from mageVerde import database, login_manager
+from flask_login import UserMixin
+
+
+@login_manager.user_loader
+def load_usuario(id_usuario):
+    return Usuario.query.get(int(id_usuario))
+
 
 class Usuario(database.Model):
     id = database.Column(database.Integer, primary_key=True)

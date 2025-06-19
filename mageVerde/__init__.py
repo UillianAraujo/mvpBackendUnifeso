d2e4c68b6470289027086464d0e44802311d91d1
UillianAraujo <uillsilva52@gmail.com>
2025-06-19 18:22:12 -0300

    import login/bcrypt no init

--- mageVerde/__init__.py
+++ mageVerde/__init__.py
@@ -1,13 +1,16 @@
 from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
+from flask_login import LoginManager
+from flask_bcrypt import Bcrypt
 
 
 app = Flask(__name__)
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///mageverde.db'
-
+app.config['SECRET_KEY'] = 'd679ee02248df5a0448ff4e4a67715cb'
 
 database = SQLAlchemy(app)
-
-
+Bcrypt = Bcrypt(app)
+login_manager = LoginManager(app)
+login_manager.login_view = 'loginUsuario'
 
 from mageVerde import routes
---
+++ testes.py
@@ -0,0 +1,6 @@
+# 'import secrets' biblioteca padrão PYTHON para gerar tokens seguros.
+# Exemplo de uso: gerar um token hexadecimal de 16 bytes.
+
+import secrets
+
+print(secrets.token_hex(16))
